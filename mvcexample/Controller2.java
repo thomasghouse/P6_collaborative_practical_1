e238d6a6a224afdecebf83a69bcffb31513ede06
Jkonopka00123 <77969615+Jkonopka00123@users.noreply.github.com>
2022-02-23 01:38:11 +0000

    Update Controller2.java
    Commented out refreshViews button and button calls
    Added incB button and calls to increment databaseB
    Changed controller2 constructor to contain window title and location

--- mvcexample/Controller2.java
+++ mvcexample/Controller2.java
@@ -11,60 +11,68 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Controller2 extends JFrame
-                         implements ActionListener {
-  
+        implements ActionListener {
+
     private Model model;
     private View3 view3;
     private View4 view4;
     private JButton clearViews;   // For direct message to views
-    private JButton refreshViews; // To prompt them to refresh their contents from the model
- 
+    //private JButton refreshViews; // To prompt them to refresh their contents from the model
+    private JButton incB; //To increment value in databaseB
+
     // Constructor
-    public Controller2(Model model) {
-    
+    public Controller2(Model model, String title, int x, int y) {
+
         // Record reference to the model
         this.model = model;
-        
+
         // Configure the window
-        setTitle("Controller2");
-        setLocation(40,200);
+        setTitle(title);
+        setLocation(x,y);
         setSize(350,150);
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         Container window = getContentPane();
         window.setLayout(new FlowLayout());     // The default is that JFrame uses BorderLayout
-        
+
         // Set up input GUI
         clearViews = new JButton("Clear views");
         window.add(clearViews);
         clearViews.addActionListener(this);
-        refreshViews = new JButton("Refresh views");
-        window.add(refreshViews);
-        refreshViews.addActionListener(this);
+//        refreshViews = new JButton("Refresh views");
+//        window.add(refreshViews);
+//        refreshViews.addActionListener(this);
+        incB = new JButton("Increment B");
+        window.add(incB);
+        incB.addActionListener(this);
         // Create views
         view3 = new View3(this, model);
         window.add(view3);
         view4 = new View4(this, model);
         window.add(view4);
-        
+
         // Display the frame
         setVisible(true);
-        
+
     } // constructor
-  
+
     // Button click handling:
     public void actionPerformed(ActionEvent e) {
-      
+
         if (e.getSource() == clearViews) {
             view3.clear();
             view4.clear();
         }
-        if (e.getSource() == refreshViews) {
-            view3.update();
-            view4.update();
+        else if(e.getSource() == incB)
+        {
+            modifyB();
         }
-        
+//        if (e.getSource() == refreshViews) {
+//            view3.update();
+//            view4.update();
+//        }
+
     } // actionPerformed
-    
+
 } // class Controller2
 
 
